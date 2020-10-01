829939322db7e152f709dccb6ba0e6e80b4f6df4
gonzo-hub <59870653+gonzo-hub@users.noreply.github.com>
2020-10-01 18:53:03 +0200

    Directorios definitivo

--- Directorios/src/Main.java
+++ Directorios/src/Main.java
@@ -1,30 +1,56 @@
 import java.io.File;
 
-public class Main {
-
+public class Ejercicio1 {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		String ruta ="C:\\Users\\Gonzotp\\Desktop\\Directorios";
+		String rutaRaiz="C:\\Users\\Gonzotp\\Desktop\\Directorios";
+		crearDirectorio(rutaRaiz, "Directorio1");
+		crearDirectorio(rutaRaiz, "Directorio2");
+		
+		crearFichero(rutaRaiz, "fichero3.txt");
+		
+		String subRuta="C:\\\\Users\\\\Gonzotp\\\\Desktop\\\\Directorios\\Directorio1";
+		crearFichero(subRuta, "fichero1.txt");
+		crearFichero(subRuta, "fichero2.txt");
 
-		
-		
-		NuevoDirectorio.crearDirectorio(ruta,"Directorio1"); 
-		NuevoDirectorio.crearDirectorio(ruta,"Directorio2");
-		
-		
-		File Directorio1 = new File("C:\\Users\\Gonzotp\\Desktopp\\Directorios");
-		
-		Directorio1.mkdir();
-		
-		File Directorio2 = new File("C:\\Users\\Gonzotp\\Desktop\\Directorios");
-				
-		Directorio2.mkdir();
+		subRuta="C:\\\\Users\\\\Gonzotp\\\\Desktop\\\\Directorios\\Directorio2";
+		crearFichero(subRuta, "fichero1.txt");
+
+	}
+
+	private static boolean crearDirectorio(String ruta, String nombre) {
+		boolean todoOK=false;
+		try {
+			File f = new File(ruta, nombre);
+			if(!f.exists()) {
+					f.mkdir();
+			}
+			else {
+				System.out.printf("El directorio '%s' ya existe. Se omite su creación\n",nombre);
+			}
+			todoOK=true;;
+		} catch (Exception ex) {
+			System.out.printf("Ha ocurrido un error al crear el directorio '%s' en la ruta '%s'\n", nombre, ruta);
+		}
+		return todoOK;
+	}
 	
-		String ruta1 ="C:\\Users\\Gonzotp\\Desktop\\Directorios\\Directorio1";
-		
-		NuevoDirectorio.crearFichero(ruta1,"Fichero1");
-		NuevoDirectorio.crearFichero(ruta1,"Fichero2");
-		
+	private static boolean crearFichero(String ruta, String nombre) {
+		boolean todoOK=false;
+		try {
+			File f = new File(ruta, nombre);
+			if(!f.exists()) {
+					if(!f.createNewFile()) {
+						System.out.printf("No se ha podido crear el fichero '%s' en la ruta '%s'\n", nombre, ruta);
+					}
+			}
+			else {
+				System.out.printf("El fichero '%s' ya existe. Se omite su creación\n",nombre);
+			}
+			todoOK=true;;
+		} catch (Exception ex) {
+			System.out.printf("Ha ocurrido un error al crear el fichero '%s' en la ruta '%s'\n", nombre, ruta);
+		}
+		return todoOK;
 	}
 }
